0f537e6caf6ec4a68887f25d36ad5c59d219daf5
rupesh <rupeshsaha899@gmail.com>
2025-07-17 09:08:11 +0530

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
 
   return (
     <>
       <div className="bg-primary min-h-screen graph-paper">
+        <Analytics/>
         <Navbar />
         <HomePage />
       </div>
